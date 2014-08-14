b0fd8869ba1c34170c8a4969f75122a0a949d826
Traun Leyden <tleyden@couchbase.com>
2014-08-14 15:17:23 -0700

    exploratory coding. add replicator2 package
    https://github.com/couchbase/couchbase-lite-android/issues/398

---
+++ src/androidTest/java/com/couchbase/lite/replicator2/ReplicationTest.java
@@ -0,0 +1,53 @@
+package com.couchbase.lite.replicator2;
+
+import com.couchbase.lite.Database;
+import com.couchbase.lite.LiteTestCase;
+import com.couchbase.lite.Manager;
+import com.couchbase.lite.util.Log;
+
+import java.net.URL;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Tests for the new state machine based replicator
+ */
+public class ReplicationTest extends LiteTestCase {
+
+    public static final String TAG = com.couchbase.lite.replicator.ReplicationTest.TAG;
+
+    /**
+     * Start continuous replication with a closed db.
+     *
+     * Expected behavior:
+     *   - Receive replication finished callback
+     *   - Replication lastError will contain an exception
+     */
+    public void testStartReplicationClosedDb() throws Exception {
+
+        Database closedDb = this.manager.getDatabase("closed");
+        closedDb.close();
+
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+        final Replication replication = new Replication(closedDb, new URL("http://fake.com/foo"));
+        replication.setContinous(true);
+        replication.addChangeListener(new Replication.ChangeListener() {
+            @Override
+            public void changed(Replication.ChangeEvent event) {
+                Log.d(TAG, "changed event: %s", event);
+                if (replication.isRunning() == false) {
+                    countDownLatch.countDown();
+                }
+
+            }
+        });
+        replication.start();
+
+        boolean success = countDownLatch.await(60, TimeUnit.SECONDS);
+        assertTrue(success);
+
+        assertTrue(replication.getLastError() != null);
+
+    }
+
+}
